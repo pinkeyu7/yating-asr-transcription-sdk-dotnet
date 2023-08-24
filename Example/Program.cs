2644ddc828b6c51e8c25f50d9f47aaf2d684ed85
pinke <pinke.yu7@gmail.com>
2023-08-25 00:40:31 +0800

    Feature: add example

---
+++ Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using YatingAsrClient;
+using YatingAsrClient.Asr;
+using YatingAsrClient.Constants;
+using YatingAsrClient.Dto;
+
+string asrApiUrl = "ASR_ENDPOINT";
+string asrApiKey = "PUT_YOUR_API_KEY_HERE";
+
+
+string audioUri = "http://www.audio.com/audio_file.wav";
+string asrModel = AsrModel.ZHTW;
+bool speakerDiarization = true;
+int speakerCount = 0;
+bool sentiment = false;
+
+
+try
+{
+    AsrClient asrClient = new AsrClient(asrApiUrl, asrApiKey);
+    Sentence[] sentences = asrClient.Transcription(audioUri, asrModel, speakerDiarization, speakerCount, sentiment);
+
+    foreach (Sentence sentence in sentences)
+    {
+        Console.WriteLine($"{sentence.GetSentence()} ");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
